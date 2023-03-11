96c6712ebe1163b55ee5233b0a6814cd22bc595a
Newmaster-70 <rliu2000@outlook.com>
2023-03-10 23:30:16 -0600

    Added Matrix Calculation Helper and Law of Cosine

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,55 @@ double pitch;
 double roll;
 double yaw;
 
+float determinant(float matrix[][10], int n) {
+  float det = 0;
+  if (n == 1) {
+    det = matrix[0][0];
+  } else if (n == 2) {
+    det = matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+  } else {
+    for (int k = 0; k < n; k++) {
+      float submatrix[10][10];
+      int i = 0;
+      for (int row = 1; row < n; row++) {
+        int j = 0;
+        for (int col = 0; col < n; col++) {
+          if (col != k) {
+            submatrix[i][j] = matrix[row][col];
+            j++;
+          }
+        }
+        i++;
+      }
+      det += pow(-1, k)*matrix[0][k]*determinant(submatrix, n-1);
+    }
+  }
+  return det;
+}
+
+// float matrix[4][4] = {{1, 2, 3, 4},
+//                       {5, 6, 7, 8},
+//                       {9, 10, 11, 12},
+//                       {13, 14, 15, 16}};
+// int n = 4;
+// float det = determinant(matrix, n);
+// Serial.print("Determinant = ");
+// Serial.println(det);
+
+double oppoLengthLawOfCosine (double a, double b, double angleInRadians){
+    return sqrt(a*a + b*b - 2*a*b*cos(angleInRadians));
+}
+
+double oppoAngleLawOfCosine(double a, double b, double c) {
+    // Calculate the cosine of the unknown angle using the law of cosines
+    double cos_angle = (a*a + b*b - c*c) / (2*a*b);
+
+    // Convert the cosine to an angle in radians using the inverse cosine function
+    double angle = acos(cos_angle);
+
+    return angle;
+}
+
 void IMU_INIT(){
         mpu.initialize();
         mpu.dmpInitialize();
